24762b86ff4fe0d8e19cc664b3ed3d44ee1884cb
Don <l_98b@outlook.com>
2022-08-29 00:22:19 +0800

    Add typescript type support

---
+++ dist/lifehash.types.d.ts
@@ -0,0 +1,7 @@
+export declare enum LifeHashVersion {
+    version1 = 0,
+    version2 = 1,
+    detailed = 2,
+    fiducial = 3,
+    grayscaleFiducial = 4
+}
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      // export the vue component
+      entry: resolve("src/main.js"),
+      name: "LifeHash-Vue",
+      // the proper extensions will be added
+      fileName: "lifehash-vue",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
